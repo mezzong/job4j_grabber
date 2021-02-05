d43979bca3ccbd44f306e1cf71aad9216ffe0934
Stas <bazhoker@gmail.com>
2021-02-06 01:11:39 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#421276]

--- src/main/java/ru/job4j/grabber/html/SqlRuParse.java
+++ src/main/java/ru/job4j/grabber/html/SqlRuParse.java
@@ -11,17 +11,21 @@ import java.time.LocalTime;
 
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
-        Elements dates = doc.select(".altCol");
-        int index = 1;
-        for (Element td : row) {
-            Element href = td.child(0);
-            Element date = dates.get(index);
-            System.out.println(href.attr("href"));
-            System.out.println(href.text());
-            System.out.println(stringToDate(date.text()));
-            index += 2;
+        int i = 1;
+        while (i < 6) {
+            Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers" + "/" + i).get();
+            Elements row = doc.select(".postslisttopic");
+            Elements dates = doc.select(".altCol");
+            int index = 1;
+            for (Element td : row) {
+                Element href = td.child(0);
+                Element date = dates.get(index);
+                System.out.println(href.attr("href"));
+                System.out.println(href.text());
+                System.out.println(stringToDate(date.text()));
+                index += 2;
+            }
+            i++;
         }
     }
 

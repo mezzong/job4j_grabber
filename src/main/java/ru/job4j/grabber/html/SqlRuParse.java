e137f0af9d4d8c9925282e15290a84ab36a83433
Stas <bazhoker@gmail.com>
2021-02-06 01:07:26 +0300

    2.1. Преобразование даты [#421280]

--- src/main/java/ru/job4j/grabber/html/SqlRuParse.java
+++ src/main/java/ru/job4j/grabber/html/SqlRuParse.java
@@ -5,6 +5,10 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
         Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
@@ -16,8 +20,37 @@ public class SqlRuParse {
             Element date = dates.get(index);
             System.out.println(href.attr("href"));
             System.out.println(href.text());
-            System.out.println(date.text());
+            System.out.println(stringToDate(date.text()));
             index += 2;
         }
+    }
+
+    public static LocalDateTime stringToDate(String date) {
+        String[] firstSplit = date.split(", ");
+        if (firstSplit.length != 2) {
+            throw new IllegalArgumentException();
+        }
+        String[] hhmm = firstSplit[1].split(":");
+        int hour = Integer.parseInt(hhmm[0]);
+        int minute = Integer.parseInt(hhmm[1]);
+        if ("сегодня".equals(firstSplit[0])) {
+            return LocalDateTime.of(LocalDate.now(), LocalTime.of(hour, minute));
+        }
+        if ("вчера".equals(firstSplit[0])) {
+            return LocalDateTime.of(LocalDate.now().minusDays(1), LocalTime.of(hour, minute));
+        }
+        String[] ddmmyy = firstSplit[0].split(" ");
+        String[] months = {"янв", "фев", "мар", "апр", "май",
+                "июн", "июл", "авг", "сен", "окт", "ноя", "дек"};
+        int year = Integer.parseInt("20" + ddmmyy[2]);
+        int month = 0;
+        int day = Integer.parseInt(ddmmyy[0]);
+        for (int i = 0; i < months.length; i++) {
+            if (months[i].equals(ddmmyy[1])) {
+                month = i + 1;
+                break;
+            }
+        }
+        return LocalDateTime.of(year, month, day, hour, minute);
     }
 }

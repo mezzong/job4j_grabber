3de3a2669d744ce72866c1272c236a5e5628617e
Stas <bazhoker@gmail.com>
2021-02-07 18:25:43 +0300

    4. Схема таблицы Post [#421283]

--- scripts/create.sql
+++ scripts/create.sql
@@ -1,7 +1,8 @@
 create table post(
     id serial primary key,
     name varchar(300),
-    text varchar(60000),
-    link varchar(300) unique,
-    created date
+    text text,
+    author varchar,
+    created timestamp,
+    link varchar(300) unique
 );
